ba3ad0b3fba25728aa67d6cbf3f22e4e9dcf7ccc
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2020-09-08 19:42:24 +0530

    Commit#4

---
+++ win_check.py
@@ -0,0 +1,12 @@
+def win_check(board,mark):
+    
+    return ((board[7] == mark and board[8] == mark and board[9] == mark) or 
+    (board[4] == mark and board[5] == mark and board[6] == mark) or
+    (board[1] == mark and board[2] == mark and board[3] == mark) or 
+    (board[7] == mark and board[4] == mark and board[1] == mark) or 
+    (board[8] == mark and board[5] == mark and board[2] == mark) or
+    (board[9] == mark and board[6] == mark and board[3] == mark) or 
+    (board[7] == mark and board[5] == mark and board[3] == mark) or 
+    (board[9] == mark and board[5] == mark and board[1] == mark))
+
+#win_check(test_board,'X')
