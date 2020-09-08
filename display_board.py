0424fd552302d1ba5f4ef6b211ef9926d7b3fb57
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2020-09-08 19:02:28 +0530

    Commit#01

---
+++ display_board.py
@@ -0,0 +1,10 @@
+def display_board(board):
+    print(test_board[9] ,'|', test_board[8] ,'|', test_board[7])
+    print('-- --- --')
+    print(test_board[6] ,'|', test_board[5] ,'|', test_board[4])
+    print('-- --- --')
+    print(test_board[3] ,'|', test_board[2] ,'|', test_board[1])
+
+test_board = ['#','X','O','X','O','X','O','X','O','X','O']    
+display_board(test_board)
+
